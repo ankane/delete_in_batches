5debabde8d5c87e42adef09ba172630fab2f9658
Sen-Zhang <solowolf21@126.com>
2016-03-13 23:08:45 -0700

    try to be DRY

--- lib/delete_in_batches.rb
+++ lib/delete_in_batches.rb
@@ -5,17 +5,9 @@ module DeleteInBatches
   def delete_in_batches(options = {})
     batch_size = options[:batch_size] || 10000
 
-    # TODO dry
-    pk = "#{quoted_table_name}.#{quoted_primary_key}"
-    sql =
-      if connection.respond_to?(:unprepared_statement)
-        # ActiveRecord 4
-        connection.unprepared_statement do
-          select(pk).limit(batch_size).to_sql
-        end
-      else
-        select(pk).limit(batch_size).to_sql
-      end
+    pk       = "#{quoted_table_name}.#{quoted_primary_key}"
+    sql_proc = proc { select(pk).limit(batch_size).to_sql }
+    sql      = connection.try(:unprepared_statement, &sql_proc) || sql_proc.call
 
     while connection.delete("DELETE FROM #{quoted_table_name} WHERE #{pk} IN (#{sql})") == batch_size
       yield if block_given?
